e6f30a1935b5be36852bc96187f895a979cd24ad
SahilMahadwar <accounts@mahadwar.com>
2024-07-01 01:49:55 +0530

    fix build error

--- src/components/project-details/all-fixtures.tsx
+++ src/components/project-details/all-fixtures.tsx
@@ -1,33 +1,6 @@
-import { DeleteOutlined, EditOutlined, PlusOutlined } from "@ant-design/icons";
-import {
-  Button,
-  Empty,
-  Flex,
-  Form,
-  Image,
-  message,
-  Spin,
-  Typography,
-} from "antd";
 import React, { useState } from "react";
-import { useFetchFixturesByProject } from "../../hooks/use-fixtures";
-import { getSpaceMeta } from "../../hooks/use-meta";
-import {
-  useDeleteSpace,
-  useFetchSpacesByProject,
-  useSaveSpace,
-} from "../../hooks/use-spaces";
-import { SpaceMeta } from "../../interfaces/Meta";
-import { ProjectDetailsProps } from "../../interfaces/Project";
-import { Space } from "../../interfaces/Space";
-import { convertInchToFeet } from "../../libs/lvnzy-helper";
-import { queryKeys } from "../../libs/react-query/constants";
-import { queryClient } from "../../libs/react-query/query-client";
-import { COLORS } from "../../styles/colors";
-import FixtureList from "../common/fixture-list";
-import { Loader } from "../loader";
-import SpaceDetails from "../space-details";
-import SlideFixtureMapping from "./slide-fixture-mapping";
+
+// WIP
 
 const ProjectFixtureDetails: React.FC<any> = ({
   projectData,
@@ -35,10 +8,10 @@ const ProjectFixtureDetails: React.FC<any> = ({
 }) => {
   return (
     <>
-      <FixtureList
+      {/* <FixtureList
         projectId={projectData?._id as string}
         fixturesUpdated={fixturesUpdated}
-      />
+      /> */}
     </>
   );
 };
